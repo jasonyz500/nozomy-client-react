2a4340e0731fd9d742d408ae5f1986b5b8c84a2d
Jason Zhou <zhou.jasony@gmail.com>
2017-10-04 22:23:25 -0700

    added a static nav at the top

---
+++ src/components/main.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Switch, Route } from 'react-router-dom'
+import Home from './home'
+import Visualize from './visualize'
+import Root from './root';
+
+const Main = () => (
+  <main>
+    <Switch>
+      <Route exact path='/' component={Home}/>
+      <Route path='/visualize' component={Visualize}/>
+      <Route path='/root' component={Root}/>
+    </Switch>
+  </main>
+)
+
+export default Main
--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap-theme.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import promise from 'redux-promise';
 
 import reducers from './reducers';
-import Home from './components/home';
-import Visualize from './components/visualize';
 
 import './index.css';
-import App from './App';
+import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 
 const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
@@ -18,11 +18,7 @@ const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
     <BrowserRouter>
-      <Switch>
-        <Route path="/visualize" component={Visualize} />
-        <Route path="/root" component={App} />
-        <Route path="/" component={Home} />
-      </Switch>
+      <App />
     </BrowserRouter>
   </Provider>
   , document.getElementById('root'));

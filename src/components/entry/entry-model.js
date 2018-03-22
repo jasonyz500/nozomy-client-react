b48fad558ebbc2c2d8d4c2af99f14a1295dc1154
Jason Zhou <zhou.jasony@gmail.com>
2018-03-22 00:09:33 -0700

    fix missing default date bug

--- src/components/entry/entry-model.js
+++ src/components/entry/entry-model.js
@@ -1,8 +1,13 @@
+import moment from 'moment';
+
 export class EntryModel {
   constructor(data) {
-    this.tags = data.tags || [];
     this.headline = data.headline || '';
+    this.tags = data.tags || [];
     this.body = data.body || '';
+    this.date_string = data.date_string || moment().format('YYYY-MM-DD');
+    this.week_string = data.week_string || moment().startOf('isoWeek').format('YYYY-MM-DD');
+    this.day_of_week_iso = data.day_of_week_iso || moment().isoWeekday();
     this._id = data._id;
   }
 }

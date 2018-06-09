5cf0fea89a794dd4e31cbc646baacfca1510cd0a
Jason Zhou <zhou.jasony@gmail.com>
2018-06-08 22:57:46 -0700

    fix bootstrap with version update

--- src/components/App.js
+++ src/components/App.js
@@ -7,10 +7,19 @@ const authService = new AuthService();
 
 class App extends Component {
   render() {
+    let mainTag;
     if (authService.isLoggedIn()) {
-      return <Main />;
+      mainTag = <Main />;
+    } else {
+      mainTag = <Login />;
     }
-    return <Login />;
+    return (
+      <div>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossOrigin="anonymous"/>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossOrigin="anonymous"/>
+        {mainTag}
+      </div>
+    );
   }
 }
 
